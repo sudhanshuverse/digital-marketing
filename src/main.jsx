9cfb452fbe5a4178f4ebcd26ff534a1cfadf1eb5
Sudhanshu Kumar <sudhanshu95084145@gmail.com>
2025-12-18 02:08:18 +0530

    Updated the main.jsx file to fix the refresh error

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { createRoot } from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
-  <HashRouter basename="/digital-marketing">
+  <BrowserRouter basename="/digital-marketing">
     <App />
-  </HashRouter>
+  </BrowserRouter>
 );
