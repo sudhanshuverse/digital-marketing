b446904163c35c2a02b694aeb807603f3131d9c2
Sudhanshu Kumar <sudhanshu95084145@gmail.com>
2025-12-18 00:47:30 +0530

    Updated docs file to live the site

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/digital-marketing">
     <App />
   </BrowserRouter>
 );
