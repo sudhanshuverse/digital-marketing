98e49c3811090cd81e60b18e7a1df8fee9fcf3bc
Sudhanshu Kumar <sudhanshu95084145@gmail.com>
2025-12-17 10:22:29 +0530

    Updated App.jsx and .\index.css and main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,28 @@
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
 import Home from "./components/Home";
+import Footer from "./components/Footer";
+import About from "./components/About";
+import Services from "./components/Services";
+import Contact from "./components/Contact";
 
 function App() {
-  return(
+  return (
     <>
       <Header />
-      <Home />
-    </>
-  )
-}
 
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<h1>404 Page Not Found</h1>} />
+      </Routes>
+
+      <Footer />
+    </>
+  );
+}
 
 export default App;
